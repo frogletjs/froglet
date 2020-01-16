efde65fede7c21170fa34874a54b41ced254521a
J. Ryan Conklin <jryanconklin@gmail.com>
2020-01-16 15:55:22 -0800

    Create CLI Scaffold

---
+++ packages/cli/lib/create.js
@@ -0,0 +1,19 @@
+// This file needs a better name.
+// The responsibility of this file will be the raw business logic of loading apps.
+
+/**
+ * Pseudocode - Create
+ * The create command should:
+ * - Add webpack configuration to root of project.
+ * - init a component library tool (Pattern Lab / Storybook) to path from user input
+ * - save path for component-library as config in froglet.config.json at root
+ * - Add application (drupal theme) to path from user input,
+ * - save path for component-library as config in froglet.config.json at root
+ *
+ */
+
+const create = function() {
+  console.log('Create Nerp');
+};
+
+module.exports = create;
